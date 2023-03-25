6ed6bc452bd7ce9713b6ae3b8d1f5a967c448796
surrim <root@surrim.org>
2023-03-25 13:11:05 +0000

    fixed spacing

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 require 'config.php.inc';
 
 $output = fopen('php://output', 'w');
-if (isset($filename)) {
-	header('Content-Type: text/csv; charset=utf-8');
-	header('Content-Disposition: attachment; filename="' . $filename . '"');
+if (!empty($filename)) {
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
 } else {
-	header('Content-Type: text/plain; charset=utf-8');
+    header('Content-Type: text/plain; charset=utf-8');
 }
 header('Pragma: no-cache');
 header('Expires: 0');
@@ -17,10 +17,9 @@ $result = $connection->query($sql);
 
 $headers = array();
 while ($fieldinfo = $result->fetch_field()) {
-	$headers[] = $fieldinfo->name;
+    $headers[] = $fieldinfo->name;
 }
 fputcsv($output, $headers, $separator ?? ',');
 while ($row = $result->fetch_array(MYSQLI_NUM)) {
-	fputcsv($output, array_values($row), $separator ?? ',');
+    fputcsv($output, array_values($row), $separator ?? ',');
 }
-
